3c4c279e52ff09988ddafb3081fc77485039a3a4
miiaharja <miia.harja@gmail.com>
2020-06-10 15:50:21 +0300

    change google maps to mapbox

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,21 +1,21 @@
 import React, { useRef, useEffect } from "react";
-
+import mapboxgl from "mapbox-gl";
+ 
 import "./Map.css";
-
+ 
 const Map = props => {
-  const mapRef = useRef();
-
   const { center, zoom } = props;
-
+  const mapRef = useRef();
   useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom
+    mapboxgl.accessToken ='pk.eyJ1IjoibWlpYWhhcmphIiwiYSI6ImNrYjU2bzhxeTBtbmYyc283dDgwcGZnYnAifQ.S9-LwEnpQAUxyZSSppxZlA';
+const map = new mapboxgl.Map({
+      container: mapRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center,
+      zoom,
     });
-
-    new window.google.maps.Marker({ position: center, map: map });
+    new mapboxgl.Marker().setLngLat(center).addTo(map);
   }, [center, zoom]);
-
   return (
     <div
       ref={mapRef}
@@ -24,5 +24,5 @@ const Map = props => {
     ></div>
   );
 };
-
+ 
 export default Map;
